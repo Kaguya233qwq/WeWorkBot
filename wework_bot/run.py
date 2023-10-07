0812462bae80217fb421b7befe17218ea65de8be
kaguya <1435608435@qq.com>
2023-10-07 20:14:09 +0800

    新增自定义插件开发系统

--- wework_bot/run.py
+++ wework_bot/run.py
@@ -4,11 +4,13 @@ import time
 import ntwork
 
 from .fuctions.config import load_config
+from .utils.loader import load_plugins_from_local
 
 
 def forever():
     try:
         load_config()
+        load_plugins_from_local()
         while True:
             time.sleep(0.5)
     except KeyboardInterrupt:
--- workbot.py
+++ workbot.py
@@ -1,29 +1,5 @@
 # -*- coding: utf-8 -*-
-import ntwork
-
-from wework_bot.fuctions.action import Action, wework
-from wework_bot.rule import Rule
 from wework_bot import run
-
-
-@wework.msg_register(ntwork.MT_RECV_TEXT_MSG)
-def msg_handler(wework_instance: ntwork.WeWork, message):
-    #  菜单命令
-    menu = (
-        ' [玫瑰]欢迎使用[玫瑰]\n'
-        ' [玫瑰]今日问候[玫瑰]\n'
-        ' [玫瑰]客户服务[玫瑰]\n'
-        ' [玫瑰]数据查询[玫瑰]\n'
-        ' [玫瑰]发送日报[玫瑰]\n'
-        ' [玫瑰]其他功能[玫瑰]'
-    )
-    Action.reply_msg(
-        wework_instance,
-        message,
-        rule=Rule.PRIVATE,
-        cmd='/菜单',
-        reply=menu
-    )
 
 
 if __name__ == '__main__':
